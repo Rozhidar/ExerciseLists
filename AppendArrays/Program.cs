7e1fe938c4700d847df4ffbb61958025ae898e86
Rozhidar <148547922+Rozhidar@users.noreply.github.com>
2025-07-31 14:31:40 +0200

    Add next project

---
+++ AppendArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> input = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries)
+    .Select(element => element.Trim()).Where(element => !string.IsNullOrWhiteSpace(element)).ToList();
+
+List<string> resultList = new List<string>();
+
+for (int i = input.Count - 1; i >= 0; i--)
+{
+    List<string> currentPart = input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+    resultList.AddRange(currentPart);
+}
+
+Console.WriteLine(string.Join(" ", resultList));
--- CardsGame/Program.cs
+++ CardsGame/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿List<int> playerOneDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> playerTwoDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int i = 0;
+while (playerOneDeck.Count > 0 && playerTwoDeck.Count > 0)
+{
+    int playerOneCurrentCard = playerOneDeck[i];
+    int playerTwoCurrentCard = playerTwoDeck[i];
+
+    playerOneDeck.Remove(playerOneCurrentCard);
+    playerTwoDeck.Remove(playerTwoCurrentCard);
+
+    if (playerOneCurrentCard > playerTwoCurrentCard)
+    {
+        playerOneDeck.Add(playerTwoCurrentCard);
+        playerOneDeck.Add(playerOneCurrentCard);
+    }
+    else if (playerOneCurrentCard < playerTwoCurrentCard)
+    {
+        playerTwoDeck.Add(playerOneCurrentCard);
+        playerTwoDeck.Add(playerTwoCurrentCard);
+    }
+}
+
+int winnerCardsSum = 0;
+
+if (playerOneDeck.Count == 0)
+{
+    winnerCardsSum = playerTwoDeck.Sum();
+    Console.WriteLine($"Second player wins! Sum: {winnerCardsSum}");
+}
+else if (playerTwoDeck.Count == 0)
+{
+    winnerCardsSum = playerOneDeck.Sum();
+    Console.WriteLine($"First player wins! Sum: {winnerCardsSum}");
+}
